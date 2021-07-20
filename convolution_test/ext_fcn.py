7c152c52fe7f08e8924c2a3bdacda03e6abfb26e
Alexander Näs <alexander.naes@gmail.com>
2021-07-20 13:28:26 +0200

    Convolution: First draft of convolution done. Problems with summing/mean. Need to understand what's going on

---
+++ convolution_test/ext_fcn.py
@@ -0,0 +1,19 @@
+
+# ************************************************
+# Function work in progress
+# ************************************************
+
+def gaussian_blur_alex(img_in):
+    print("Adding gaussian blur...")
+    img_filtered = img_in
+
+    imWidth = len(img_filtered[0, :])
+    imHeight = len(img_filtered[:, 0])
+
+    print("mod image width: " + str(imWidth))
+    print("mod image height: " + str(imHeight))
+
+    # Perform convolution
+    # BLABLABLA
+
+    return img_filtered
